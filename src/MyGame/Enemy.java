359d0930ae663d14f3e196b0d1e68ab778dfee42
KimYongHun27 <ryh9911@gmail.com>
2026-01-30 17:14:51 +0900

    avatar_enemy_constructor

--- src/MyGame/Enemy.java
+++ src/MyGame/Enemy.java
@@ -1,5 +1,11 @@
 package MyGame;
 
-public class Enemy {
+public class Enemy extends Character {
+	
+	
+	public Enemy(String name, int hp, int att, int def)
+	{
+		super(name, hp, att, def);
+	}
 
 }
